d02f306529abf6abc3de68c892d201e4ef0476a6
leon.bollerup@gmail.com <leon.bollerup@gmail.com@c7ebc68b-1786-6683-9efc-5701743e9178>
2013-01-01 19:30:58 +0000

    Added "kextname" to ps2db for use with version checking and added some new fancy shit code to show versions for ps2 kexts while doing build

--- trunk/bin/ps2db.inc.php
+++ trunk/bin/ps2db.inc.php
@@ -6,54 +6,63 @@ $ps2db = array(
 
 		array( 	name 		=> "myHack default", 
                 foldername 	=> "myhackps2",
+                kextname	=> "ApplePS2Controller.kext",
                 arch		=> "x86_x64",
                 notes 		=> "myHack 3.1 default ps2 kexts"
              ),
                          
 		array( 	name 		=> "Apple's PS2 controller", 
                 foldername 	=> "appleps2controller",
+                kextname	=> "ApplePS2Controller.kext",                
                 arch		=> "x86",
                 notes		=> "Standard package for many ALPS pads, have support for 2 finger scroll and the track point works"
              ),
 
         array(  name 		=> "VoodooPS2Controller (debug version; suited for Dell D4x0/D520)",
                 foldername 	=> "vps2c-debug",
+                kextname	=> "VoodooPS2Controller.kext", 
 			    arch		=> "x86",
                 notes		=> "General good package for ALPS pads, side/bottom scroll works, mouse works great"
              ),  
 
         array(  name 		=> "Slice's ApplePS2Controller",
                 foldername 	=> "slicePS2Controller",
+                kextname	=> "ApplePS2Controller.kext",                 
                 arch    	=> "x86",
                 notes	 	=> "Works very well for most trackpads, its a beta driver, so there might be unknown bugs"
              ),  
 
         array(  name 		=> "ANV-Slice Modified VoodooPS2Controller",
                 foldername 	=> "ANV-SliceModified",
+                kextname	=> "ApplePS2Controller.kext",                
                 arch    	=> "x86_x64",
                 notes	 	=> "Allthough its a beta driver it works well on most ALPS pads, scrolling is a little too fast"
              ),  
 
         array(  name 		=> "Apple's PS2 controller for Dell E generation laptops",
                 foldername 	=> "appleps2controller2",
+                kextname	=> "ApplePS2Controller.kext",                 
                 arch    	=> "x86_x64",
                 notes	 	=> "It works on the Dell e6220, no multi touch, gestures or on pad scrolling"
              ),  
              
         array(  name 		=> "Apple's PS2 controller for Samsung R530",
                 foldername 	=> "appleps2controller3",
+                kextname	=> "ApplePS2Controller.kext",                 
                 arch    	=> "x86_x64",
                 notes	 	=> "PS2controller, made for the Samsung R530 ALPS."
              ),   
                        
         array(  name 		=> "Apple's PS2 controller for Dell E65xx",
                 foldername 	=> "appleps2controller3",
+                kextname	=> "ApplePS2Controller.kext",                 
                 arch    	=> "x86_x64",
                 notes	 	=> "It works on the Dell E6520, no multi touch, gestures or on pad scrolling"
              ),        
 
         array(  name 		=> "VoodooPS2controller #1",
                 foldername 	=> "voodoops2-1",
+                kextname	=> "VoodooPS2Controller.kext",                
                 arch    	=> "x86",
                 notes	 	=> "Works good with Synaptics with both 2 fingers etc."
              ),
