eff27c97d5d617d03a7a9a9f5607cfe366833ae0
leon.bollerup <leon.bollerup@gmail.com>
2013-02-13 14:21:45 +0000

    Bug fix in web interface - removing some testing code

--- bin/html/index.php
+++ bin/html/index.php
@@ -5,6 +5,7 @@
 	include "header.inc.php";
 	include_once "../config.inc.php";
 	global $edpmode; $edpmode = "web";
+	$donateurl = "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=mail%40r2x2%2ecom&lc=US&item_name=OSXlatitude%20Donation&item_number=OSXLatitude%20Donation&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHostedGuest";
 ?>
 
 <body onload="bootloader();" onresize="aligndesign();">
@@ -12,13 +13,13 @@
 
 <table border="0" width="100%" cellpadding="0" style="border-collapse: collapse" background="images/topbar-bg-normal2.png" height="69">
 	<tr style="vertical-align: bottom;"  class="topbarmenu" align="center" style='cursor: hand'>
-		<td width="90" onclick="selectT('1'); load('index.php?i=Configuration')"><img src="icons/sysprefs.png" width="40"></td>
+		<td width="90" onclick="load('index.php?i=Configuration')"><img src="icons/sysprefs.png" width="40"></td>
 		<td width="90" onclick="load('index.php?i=Fixes')"><img src="icons/fixes.png" width="38"></td>
 		<td width="90" onclick="load('index.php?i=Tools')"><img src="icons/tools.png" width="38"></td>
 		<td width="90" onclick="loadModule('workerapp.php?action=update-edp');"><img src="icons/Installer.png" width="38"></td>
 		<td width="90" onclick="loadModule('workerapp.php?action=changelog');"><img src="icons/changelog.png" width="38"></td>
 		<td>&nbsp;</td>
-		<td width="90" onclick="window.open('https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=mail%40r2x2%2ecom&lc=US&item_name=OSXlatitude%20Donation&item_number=OSXLatitude%20Donation&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHostedGuest')"><img src="icons/paypal.png" width="38"></td>
+		<td width="90" onclick="window.open('<?="$donateurl";?>')"><img src="icons/paypal.png" width="38"></td>
 	</tr>
 	<tr class="topbarmenu" align="center" style='cursor: hand'>
 		<td onclick="load('index.php?i=Configuration')">Config</td>
@@ -27,7 +28,7 @@
 		<td onclick="load('index.php?i=Configuration')">Update</td>
 		<td onclick="loadModule('workerapp.php?action=changelog');">Changelog</td>
 		<td>&nbsp;</td>
-		<td onclick="window.open('https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=mail%40r2x2%2ecom&lc=US&item_name=OSXlatitude%20Donation&item_number=OSXLatitude%20Donation&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHostedGuest')">Donate</td>
+		<td onclick="window.open('<?="$donateurl";?>')">Donate</td>
 	</tr>
 </table>
 
