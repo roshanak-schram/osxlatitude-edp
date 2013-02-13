a31e81486127db1cf3088bbab5c8cc89eca230b0
RoyalNoob3 <RoyalNoob3@gmail.com>
2013-02-13 20:25:47 +0000

    Adding support for the Atheros AR9462! :)

--- bin/wifidb.inc.php
+++ bin/wifidb.inc.php
@@ -4,13 +4,23 @@ global $wifidb;
 $wifidb = array( 
 
 		array( 	
-				name 		=> "Atheros #01", 
+				name 		=> "Atheros - AR5B195", 
                 foldername 	=> "atheros01",
                 kextname	=> "AirPortAtheros40.kext",
                 arch		=> "x86_x64",
                 supported	=> "Dell DW1702 / Atheros AR5B195",
                 supporturl	=> "http://forum.osxlatitude.com/index.php?/topic/1916-the-perfect-mini-pcie-combo-card",
                 notes 		=> "Read on the support URL for how to enable bluetooth support"
+             ),
+             
+        array( 	
+				name 		=> "Atheros - AR9462", 
+                foldername 	=> "Atheros-AR9462",
+                kextname	=> "AirPortAtheros40.kext",
+                arch		=> "x86_x64",
+                supported	=> "Atheros - AR9462",
+                supporturl	=> "",
+                notes 		=> ""
              ),
                          
                                                                              
