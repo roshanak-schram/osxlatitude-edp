65d7b68e184ad6d79c1c3056dafb1d7507ce8cdf
leon.bollerup <leon.bollerup@gmail.com>
2013-02-19 17:33:29 +0000

    Removing old EDPweb.app

--- bin/html/header.inc.php
+++ bin/html/header.inc.php
@@ -14,6 +14,19 @@
 <link href="css/developer-style.css" rel="stylesheet" media="screen" type="text/css" />
 <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
 <script src="javascript/functions.js" type="text/javascript"></script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38629435-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 <title>EDP <?="v$edpversion - Rev: $localrev";?></title>
 
 </head>
