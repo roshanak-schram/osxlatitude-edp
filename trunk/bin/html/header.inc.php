c3a3a14362e3e1474e744fed53195243f4999420
leon.bollerup@gmail.com <leon.bollerup@gmail.com@c7ebc68b-1786-6683-9efc-5701743e9178>
2013-02-19 17:22:08 +0000

    Commiting new favicon along with code change to support favicon

--- trunk/bin/html/header.inc.php
+++ trunk/bin/html/header.inc.php
@@ -13,6 +13,7 @@
 <link href="pics/homescreen.gif" rel="apple-touch-icon" />
 <meta content="minimum-scale=1.0, width=device-width, maximum-scale=0.6667, user-scalable=no" name="viewport" />
 <link href="css/developer-style.css" rel="stylesheet" media="screen" type="text/css" />
+<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
 <script src="javascript/functions.js" type="text/javascript"></script>
 <title>EDP <?="v$edpversion - Rev: $localrev";?></title>
 
