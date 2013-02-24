8a5f1a7fcf5b4d7943b2d11f1fc4ebabcf0e890e
leon.bollerup <leon.bollerup@gmail.com>
2013-02-24 18:26:39 +0000

    Adding Abyss web server to make EDP compatible with 32bit crap

--- bin/edptool.sh
+++ bin/edptool.sh
@@ -1,5 +1,6 @@
 cd /Extra/bin
-killall php
-./php -S 127.0.0.1:11250 -t /Extra/bin/html &
+sudo open httpd.app
+#killall php
+# ./php -S 127.0.0.1:11250 -t /Extra/bin/html &
 sleep 3;
 open /Extra/storage/apps/EDPweb.app
