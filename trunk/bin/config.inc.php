aac13dc7be93f34ef2006087776735b8c715db3b
leon.bollerup@gmail.com <leon.bollerup@gmail.com@c7ebc68b-1786-6683-9efc-5701743e9178>
2013-02-25 20:09:01 +0000

    *grrrr* .. paths not being picked up right...

--- trunk/bin/config.inc.php
+++ trunk/bin/config.inc.php
@@ -1,11 +1,11 @@
 <?php
 
 	//SQLite stuff :)
-	$edp_db = new PDO("sqlite:edp.sqlite3");
+	$edp_db = new PDO("sqlite:/Extra/bin/edp.sqlite3");
 	$edp_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 	//Include general functions
-	include_once "functions.inc.php";
+	include_once "/Extra/bin/functions.inc.php";
 	
 
 	//Get Vars from config storage
