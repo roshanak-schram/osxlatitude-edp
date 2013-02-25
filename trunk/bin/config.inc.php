89d3b45c8562b3c60e279aa0a0a11b511e48d383
leon.bollerup@gmail.com <leon.bollerup@gmail.com@c7ebc68b-1786-6683-9efc-5701743e9178>
2013-02-25 20:00:34 +0000

    Adding wifi copy to build process and cleaning up config

--- trunk/bin/config.inc.php
+++ trunk/bin/config.inc.php
@@ -1,43 +1,17 @@
 <?php
-	$workpath	= "/Extra";
-	$edpversion = "5.1";
-	date_default_timezone_set('UTC');
-	
-	include_once "$workpath/bin/functions.inc.php";
-	
-	$verbose 	= "true";
 
-
-
-	//Get system vars
-	//$workpath	= getenv('PWD');
-	$edpmode	= "";
-	$ee			= "$workpath/Extensions";
-	$strip		= str_replace("/", "", "$workpath");		
-	$rootpath	= str_replace("Extra", "", "$workpath");
-	$slepath	= "".$rootpath."System/Library/Extensions";
-	$cachepath	= "".$rootpath."System/Library/Caches/com.apple.kext.caches/Startup";
-	$incpath	= "$workpath/include";
-	
-    $localrev	= exec("cd $workpath; svn info --username osxlatitude-edp-read-only --non-interactive | grep -i \"Last Changed Rev\"");
-    $localrev	= str_replace("Last Changed Rev: ", "", $localrev);
-    $hibernatemode = exec("pmset -g | grep hibernatemode"); $hibernatemode = str_replace("hibernatemode", "", $hibernatemode); $hibernatemode = str_replace(" ", "", $hibernatemode);
- 
- 	$os_string  = "";
-	$os			= getVersion();	
-	$verbose 	= "1";
-
-
-	$version 	= "Rev: $localrev";
-	$header 	= "-- EDP v$edpversion - Rev: $localrev ------------------------------------------------------------------";
-	$footer		= "---------------------------------------------------------------------------- O S X L A T I T U D E . C O M --";
-	
-
+	//SQLite stuff :)
 	$edp_db = new PDO("sqlite:$workpath/bin/edp.sqlite3");
 	$edp_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-		    
-	//Generate multi-dims - this is instead of re-writting all code and to keep support for console mode
+	
+	function getConfig($name) {
+		global $edp_db;
+		$stmt = $edp_db->query("SELECT * FROM config where name = '$name'");
+		$stmt->execute();
+		return "$bigrow[0]['value']";
+	}
+		
+	//Generate multidimensional arrays - this is instead of re-writting all code and to keep support for console mode
 		//audio
 		$stmt = $edp_db->query("SELECT * FROM audio order by id");
 		$stmt->execute(); $audiodb = $stmt->fetchAll();
@@ -46,14 +20,51 @@
 		$stmt->execute(); $batterydb = $stmt->fetchAll();		
 		//Ethernet
 		$stmt = $edp_db->query("SELECT * FROM ethernet order by id");
-		$stmt->execute(); $landb = $stmt->fetchAll();		
+		$stmt->execute(); $landb = $stmt->fetchAll();
+		//wifi
+		$stmt = $edp_db->query("SELECT * FROM wifi order by id");
+		$stmt->execute(); $wifidb = $stmt->fetchAll();		
 		//ps2
 		$stmt = $edp_db->query("SELECT * FROM ps2 order by id");
 		$stmt->execute(); $ps2db = $stmt->fetchAll();		
 		
 		
-				
+		
+
+
+	//Path of EDP
 	
+	
+	//Get Vars from config storage
+	$edpversion = getConfig('edpversion');
+	$verbose 	= getConfig('verbose');
+	
+	$workpath	= getConfig('workpath');
+	$ee			= getConfig('ee');
+	$rootpath	= getConfig('rootpath');
+	$slepath	= getConfig('slepath');
+	$cachepath	= getConfig('cachepath');
+	$incpath	= getConfig('incpath');
+	
+	date_default_timezone_set('UTC');
+	
+	include_once "$workpath/bin/functions.inc.php";
+	
+	
+
+
+	//Get system vars
+	//$workpath	= getenv('PWD');  //Old detection.. not used anymore.. but we'll keep it around for now....
+    $localrev	= exec("cd $workpath; svn info --username osxlatitude-edp-read-only --non-interactive | grep -i \"Last Changed Rev\"");
+    $localrev	= str_replace("Last Changed Rev: ", "", $localrev);
+    $hibernatemode = exec("pmset -g | grep hibernatemode"); $hibernatemode = str_replace("hibernatemode", "", $hibernatemode); $hibernatemode = str_replace(" ", "", $hibernatemode);
+
+    //OS version detection stuff
+ 	$os_string  = "";
+	$os			= getVersion();	
+	$version 	= "Rev: $localrev";
 	if ($os == "") { echo "Unable to detect operating system, edptool has exited"; exit; }
+	
+
 
 ?>
