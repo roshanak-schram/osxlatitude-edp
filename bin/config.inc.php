91b5de29953345d8ebb400aa5484d14fef789fc3
leon.bollerup <leon.bollerup@gmail.com>
2013-02-28 19:16:15 +0000

    Added a neat little function from mick to show if there is updates via the badge on edpweb

--- bin/config.inc.php
+++ bin/config.inc.php
@@ -43,14 +43,17 @@
 	date_default_timezone_set('UTC');
 	
 
-	
-	
 
 
 	//Get system vars
 	//$workpath	= getenv('PWD');  //Old detection.. not used anymore.. but we'll keep it around for now....
-    $localrev	= exec("cd $workpath; svn info --username osxlatitude-edp-read-only --non-interactive | grep -i \"Last Changed Rev\"");
-    $localrev	= str_replace("Last Changed Rev: ", "", $localrev);
+    $localrev		= exec("cd $workpath; svn info --username osxlatitude-edp-read-only --non-interactive | grep -i \"Last Changed Rev\"");
+    $localrev		= str_replace("Last Changed Rev: ", "", $localrev);
+	$remoterev      = exec("cd $workpath; svn info -r HEAD --username edp --password edp --non-interactive | grep -i \"Last Changed Rev\"");
+    $remoterev      = str_replace("Last Changed Rev: ", "", $remoterev);  
+    $number_updates = ($remoterev - $localrev);
+    
+
     $hibernatemode = exec("pmset -g | grep hibernatemode"); $hibernatemode = str_replace("hibernatemode", "", $hibernatemode); $hibernatemode = str_replace(" ", "", $hibernatemode);
 
     //OS version detection stuff
