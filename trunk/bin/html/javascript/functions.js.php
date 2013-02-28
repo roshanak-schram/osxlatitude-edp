1b16235ff539d1f326012b88196f2cb54ede278c
leon.bollerup@gmail.com <leon.bollerup@gmail.com@c7ebc68b-1786-6683-9efc-5701743e9178>
2013-02-28 19:42:43 +0000

    More debugging

--- trunk/bin/html/javascript/functions.js.php
+++ trunk/bin/html/javascript/functions.js.php
@@ -1,9 +1,11 @@
+<?php
+	include_once "../config.inc.php";
+?>
 var iWebkit;if(!iWebkit){iWebkit=window.onload=function(){function fullscreen(){var a=document.getElementsByTagName("a");for(var i=0;i<a.length;i++){if(a[i].className.match("noeffect")){}else{a[i].onclick=function(){window.location=this.getAttribute("href");return false}}}}function hideURLbar(){window.scrollTo(0,0.9)}iWebkit.init=function(){fullscreen();hideURLbar()};iWebkit.init()}}
 
 
 function checkUpdates() {
 	<?php
-		global $localrev; global $workpath;
 		$remoterev      = exec("cd $workpath; svn info -r HEAD --username edp --password edp --non-interactive | grep -i \"Last Changed Rev\"");
 		$remoterev      = str_replace("Last Changed Rev: ", "", $remoterev);
 		$number_updates = ($remoterev - $localrev);	
