b7bfb040126a00fb6e1a9b1c58c17a869f864a86
leon.bollerup <leon.bollerup@gmail.com>
2013-02-28 19:38:36 +0000

    Moving javascript functions to a global lib

--- bin/html/index.php
+++ bin/html/index.php
@@ -45,38 +45,7 @@ $donateurl = "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=mail
         <script charset="utf-8">
             function bootloader() {
                 aligndesign();
-            }
-            function aligndesign() {
-                if (document.body && document.body.offsetWidth) {
-                    w = document.body.offsetWidth;
-                    h = document.body.offsetHeight;
-                }
-                if (document.compatMode == 'CSS1Compat' &&
-                        document.documentElement &&
-                        document.documentElement.offsetWidth) {
-                    w = document.documentElement.offsetWidth;
-                    h = document.documentElement.offsetHeight;
-                }
-                if (window.innerWidth && window.innerHeight) {
-                    w = window.innerWidth;
-                    h = window.innerHeight;
-                }
-
-                //Calculate and correction of the console_iframe
-                var console_iframe_width = w - 285;
-                var console_iframe_height = h - 69;
-                document.getElementById('console_iframe').style.width = console_iframe_width + 'px';
-                document.getElementById('console_iframe').style.height = console_iframe_height + 'px';
-
-                //Calculate and correction of menu dev
-                var edpmenu_div_height = h - 69;
-                document.getElementById('edpmenu').style.height = edpmenu_div_height + 'px';
-            }
-            function load(page) {
-                document.location.href = page;
-            }
-            function loadModule(page) {
-                top.document.getElementById('console_iframe').src = page;
+                checkUpdates();
             }
         </script>
     </body>
