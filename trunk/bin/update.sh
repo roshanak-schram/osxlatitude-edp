c678632e09a7bd138f70f4ec965d1bc14453b5e5
leon.bollerup@gmail.com <leon.bollerup@gmail.com@c7ebc68b-1786-6683-9efc-5701743e9178>
2013-03-06 19:36:16 +0000

    bugfix in console updater

--- trunk/bin/update.sh
+++ trunk/bin/update.sh
@@ -1,5 +1,5 @@
 echo Cleaning up /Extra using SVN \n
 svn cleanup /Extra
-echo \nDownloading latest sources from EDP's svn server \n
+echo Downloading latest sources from EDP's svn server \n
 svn --non-interactive --username edp --password edp --force update /Extra
 chmod -R 755 /Extra
