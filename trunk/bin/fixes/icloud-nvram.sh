10ac17c2e6cfde7e4e2b69e3c4a7971b5255b266
RoyalNoob3@gmail.com <RoyalNoob3@gmail.com@c7ebc68b-1786-6683-9efc-5701743e9178>
2013-03-08 15:21:38 +0000

    It's bug patching day! Another bug patched.

--- trunk/bin/fixes/icloud-nvram.sh
+++ trunk/bin/fixes/icloud-nvram.sh
@@ -1,22 +1,22 @@
 #! /bin/sh
-echo "This fix will fix the iCloud Find my Mac problem.\n"
-echo "You need to put the check on manually!"
+echo "This fix will fix the iCloud Find my Mac problem."
+echo "You need to put the check on manually!\n"
 sleep 2
 cd /Library/LaunchDaemons
-
 sudo cp /Extra/bin/fixes/storage/com.nvram.root.plist /Library/LaunchDaemons/com.nvram.root.plist
 sudo chown root:wheel /Library/LaunchDaemons/com.nvram.root.plist
 
 # Put iCloud fix on!
-echo "Go to iCloud and check in Find My Mac. You have 1 minute to do this!\n\n"
+echo "Go to iCloud and check in Find My Mac. You have 1 minute to do this!"
+echo "After this menu, the script will automatically continue.\n"
 sleep 60
 # Clear nvram
 sudo nvram -p > /var/log/nvram.log
 
-# Remove file nvram from Extra folder
 cd /Extra/
 sudo rm nvram*
 
 # Finished!
-echo "nvram file created! You can restart your computer now!\n"
-echo "After restart verify that Find My Mac check box is still selected. If so the edit is working successfully."
+echo "\nnvram file created! You can restart your computer now!\n"
+echo "After restart verify that Find My Mac check box is still selected. 
+echo "If so the edit is working successfully."
