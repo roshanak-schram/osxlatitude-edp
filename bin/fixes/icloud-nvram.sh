2a86a15dfedf52b086a2189e6aeee747fff7e7fe
RoyalNoob3 <RoyalNoob3@gmail.com>
2013-03-08 15:10:15 +0000

    Bugfix iCloud script.

--- bin/fixes/icloud-nvram.sh
+++ bin/fixes/icloud-nvram.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 echo "This fix will fix the iCloud Find my Mac problem.\n"
 echo "You need to put the check on manually!"
-sleep (2)
+sleep 2
 cd /Library/LaunchDaemons
 
 sudo cp storage/com.nvram.root.plist /Library/LaunchDaemons/com.nvram.root.plist
@@ -9,7 +9,7 @@ sudo chown root:wheel /Library/LaunchDaemons/com.nvram.root.plist
 
 # Put iCloud fix on!
 echo "Go to iCloud and check in Find My Mac. You have 1 minute to do this!\n\n"
-sleep (60)
+sleep 60
 # Clear nvram
 sudo nvram -p > /var/log/nvram.log
 
