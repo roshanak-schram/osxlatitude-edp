6b888e98a51dfd36138431571ef87ec42b9b814f
RoyalNoob3 <RoyalNoob3@gmail.com>
2013-03-11 22:45:27 +0000

    Fix.

--- bin/html/showresource.php
+++ bin/html/showresource.php
@@ -10,21 +10,17 @@ $id = $_GET['id'];
 $stmt = $edp_db->query("SELECT * FROM resources where id = '$id'");
 $stmt->execute();
 $bigrow = $stmt->fetchAll(); $row = $bigrow[0];
-		
-		
+	
 ?>
-
 
 <div class="pageitem_top">
 <img src="icons/installer.png"><span><?="$row[name]";?></span>
 </div>
 <div class="pageitem_bottom">
-
 	<?="$row[description]";?>
-
 </div>
 <ul class="pageitem">
-<li class="button"><input name="Submit input" type="submit" onclick="document.location.href='<?="$row[action]";?>'" value="OK, I understand.. go ahead!" /></li>
+<li class="button"><input name="Submit input" type="submit" onclick="document.location.href='<?="$row[action]";?>'" value="Next!" /></li>
 </ul>
 
 
