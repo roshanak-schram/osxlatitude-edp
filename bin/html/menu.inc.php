1a80851dad8b521d7b6797649e0d8d433dc5c081
leon.bollerup <leon.bollerup@gmail.com>
2013-03-11 20:47:10 +0000

    Another major design update - Now we have categories in sidebar also replaced many topbar icons

--- bin/html/menu.inc.php
+++ bin/html/menu.inc.php
@@ -1,6 +1,7 @@
 <?php
 	$i = $_GET['i'];
 	include_once "../config.inc.php";
+	if (!$i) { $i = "EDP"; }
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -9,26 +10,52 @@
 	<link href="css/sidebar.css" rel="stylesheet" media="screen" type="text/css" />
 </head>
 <body background="images/sidebar_bg.png">
-<div id="title" class="edpmenu_title_text"><? echo "$i"; ?></div>
 
-<table id="menu" class="edpmenu_menuoption" border="0" width="100%" cellpadding="0" style="border-collapse: collapse">
-		
+
+<? //Write out the standard menu - un-categorized items ?>
+<div id='title' class='edpmenu_title_text' style='margin-top: 10px'>&nbsp;&nbsp;<?="$i";?></div>
+<? generateMenu("$i", ""); ?>
+
+
 <?php
-//Connect to DB.resources and fetch menu items
-if ($i == "") { $i = "Configuration"; }
-$result = $edp_db->query("SELECT * FROM resources where menu = '$i' order by name");
-foreach($result as $row) {
-	if ($row[status] == "active") {
-		addMenuItem("loadModule('showresource.php?id=$row[id]');", "icons/sidebar/$row[icon]", "$row[name]");
+//Fetch menu items that have a category defined 
+$result1 = $edp_db->query("SELECT * FROM resources where menu = '$i' order by category");
+foreach($result1 as $row1) {
+	if ($row1[category] != $last) { 
+		//$result2 = $edp_db->query("SELECT * FROM resources where menu = '$i' AND category = '$row1[category]' order by name");
+		echo "<div id='title' class='edpmenu_title_text'>&nbsp;&nbsp;$row1[category]</div>";
+		generateMenu("$i", "$row1[category]");
 	}
+	$last = $row1[category];
+}	
+
+
+
+function generateMenu($menu, $category) {
+	global $edp_db;
+	echo "<table id='menu' class='edpmenu_menuoption' border='0' width='100%' cellpadding='0' style='border-collapse: collapse'>\n";
+	
+	$result = $edp_db->query("SELECT * FROM resources where menu = '$menu' order by name");
+	foreach($result as $row) {
+		if ($row[category] == "$category" && $row[status] == "active") {
+			//Check if the type is redirect (meaning it will go thru showresource for direct where it will use ACTION from db instead
+			if ($row[type] == "direct") { addMenuItem("loadModule('$row[action]');", "icons/sidebar/$row[icon]", "$row[name]"); }
+			else { addMenuItem("loadModule('showresource.php?id=$row[id]');", "icons/sidebar/$row[icon]", "$row[name]"); }
+		}
+	}
+	echo "</table><br>";
+}	
+
+
+function addMenuItem($action, $icon, $title) {
+	echo "<tr onclick=\"$action\" style='cursor: hand'>";
+	echo "	<td width='20' height='28'></td>\n";
+	echo "	<td width='24' height='28'><img alt='list' src='$icon' width='18px' height='18px'/></td>\n";
+	echo "	<td><span class='edpmenu_menuoption_text' onmouseover='mover(this)' onmouseout='mout(this)'>$title</span></td>";
+	echo "</tr>\n";
 }
 
-			
-								
-	function addMenuItem($action, $icon, $title) {
-		echo "<tr onclick=\"$action\" style='cursor: hand'><td width='24' height='30'><img alt='list' src='$icon' width='18px' height='18px'/></td>\n";
-		echo "<td><span class='edpmenu_menuoption_text' onmouseover='mover(this)' onmouseout='mout(this)'>$title</span></td></tr>\n";
-	}
+
 ?>
 		
 		
@@ -41,7 +68,7 @@ foreach($result as $row) {
 	}
 </script>
 	
-</table>
+
 
 
 <script>
---
+++ bin/html/pages/edp.inc.php
@@ -0,0 +1,104 @@
+<?php
+include_once "libs/Highchart.php";
+
+include_once "../config.inc.php";
+
+//Get data from db
+$result = $edp_db->query("SELECT `vendor`, COUNT(*) AS `count` FROM `models` GROUP BY `vendor` ORDER BY `count` DESC");
+
+$vendors = array();
+$total = 0;
+// Loop the result and add it to $vendors
+foreach($result as $row) {
+	$tmp = $row;
+	unset($tmp[0], $tmp[1]);
+	$vendors[] = $tmp;
+	$total += $row['count'];
+}
+// Calculate the percentage per vendor
+foreach($vendors as $i => $vendor) {
+	if($total != 0) {
+		$c = round(($vendor['count']/$total)*100, 1);
+		$vendors[$i]['share'] = round($c, 0);
+	} else {
+		$vendors[$i]['share'] = 0;
+	}
+}
+
+
+$chart = new Highchart();
+
+$chart->chart->renderTo = "container";
+$chart->chart->plotBackgroundColor = '#FFFFFF';
+$chart->chart->plotBorderWidth = '0px';
+$chart->chart->plotShadow = false;
+$chart->title->text = "We currently have $total systems in EDP";
+
+$chart->tooltip->formatter = new HighchartJsExpr("function() {
+    return '<b>'+ this.point.name +'</b>: '+ Math.round(this.percentage) +' %';}");
+
+$chart->plotOptions->pie->allowPointSelect = 1;
+$chart->plotOptions->pie->cursor = "pointer";
+$chart->plotOptions->pie->dataLabels->enabled = 1;
+$chart->plotOptions->pie->dataLabels->color = "#000000";
+$chart->plotOptions->pie->dataLabels->connectorColor = "#000000";
+
+$chart->plotOptions->pie->dataLabels->formatter = new HighchartJsExpr("function() {
+    return '<b>'+ this.point.name +'</b>: '+ Math.round(this.percentage) +' %'; }");
+
+
+
+
+
+// Make the array for the chart
+$vendor_chart_data = array();
+foreach($vendors as $vendor) {
+	$vendor_chart_data[] = array(
+		'name' => $vendor['vendor'],
+		'y' => $vendor['share'],
+		'sliced' => $vendor['vendor'] == "Dell" ? TRUE : FALSE,
+	);
+}
+$vendor_chart = array(
+	'type' => "pie",
+	'name' => "Vendor share",
+	'data' => $vendor_chart_data,
+);
+
+
+$chart->series[] = $vendor_chart;
+
+
+      foreach ($chart->getScripts() as $script) {
+         echo '<script type="text/javascript" src="' . $script . '"></script>';
+      }
+    ?>
+<p><span class='graytitle'>Welcome to EDP..</span>
+	<br><br>
+	<ul class="pageitem">
+		<li class="textbox"><p>EDP is a uniq control panel for your hackintosh that makes it easy to maintain and configure your machine - EDP's internal database contains 'best practice' schematics for 50+ systems - this makes it easy to choose the right configuration</p>
+		</li>
+	</ul>
+</p>
+	<br><br>
+
+    <div id="container"></div>
+    <script type="text/javascript">
+    <?php
+      echo $chart->render("chart1");
+    ?>
+    
+ //Hack: Modify color of pie chart border   
+ $(document).ready(function() {
+$( "rect" ).each(function( index ) {
+  if($(this).attr("fill")=="#FFFFFF") { 
+	  $(this).attr("fill", "#FFFFFF");
+  }
+});
+ });
+ 
+ //Hack hide the watermark
+ document.getElementById('watermark').style.display = 'none';
+ 
+ 
+    </script>
