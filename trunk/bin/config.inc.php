554c329bf4a6e7cf6e8b39a6f1df808f1f8ff88f
leon.bollerup@gmail.com <leon.bollerup@gmail.com@c7ebc68b-1786-6683-9efc-5701743e9178>
2013-04-06 08:30:25 +0000

    Removing session system

--- trunk/bin/config.inc.php
+++ trunk/bin/config.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-// Main path
+// Main vars
 $workpath = "/Extra";
 
 // SQLite stuff :)
@@ -8,35 +8,49 @@ $edp_db = new PDO("sqlite:/$workpath/bin/edp.sqlite3");
 $edp_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
+$edpversion = getConfig('edpversion');
+$verbose    = getConfig('verbose');	
+$ee         = getConfig('ee');			
+$rootpath   = getConfig('rootpath');
+$slepath    = getConfig('slepath');	
+$cachepath  = getConfig('cachepath');
+$incpath    = getConfig('incpath');
 
-//Initiate preloader
-session_start();
-//$remoterev 		= $_SESSION['remoterev'];
-$number_updates = $_SESSION['number_updates'];
-$audiodb		= $_SESSION['audiodb'];
-$batterydb		= $_SESSION['batterydb'];		
-$landb			= $_SESSION['landb'];
-$wifidb			= $_SESSION['wifidb'];
-$ps2db			= $_SESSION['ps2db'];
 
-$edpversion		= $_SESSION['edpversion'];
-$verbose		= $_SESSION['verbose'];
-$ee				= $_SESSION['ee'];
-$rootpath		= $_SESSION['rootpath'];				
-$slepath		= $_SESSION['slepath'];
-$cachepath		= $_SESSION['cachepath'];
-$incpath		= $_SESSION['incpath'];
 
+
+//Populate Audio array
+$stmt = $edp_db->query("SELECT * FROM audio order by id");
+$stmt->execute(); $audiodb = $stmt->fetchAll();
+
+//Populate Battery array
+$stmt = $edp_db->query("SELECT * FROM battery order by id");
+$stmt->execute(); $batterydb = $stmt->fetchAll();
+
+//Populate Ethernet array
+$stmt = $edp_db->query("SELECT * FROM ethernet order by id");
+$stmt->execute(); $landb = $stmt->fetchAll();
+$_SESSION['landb'] = $landb;
+
+//Populate Wifi array
+$stmt = $edp_db->query("SELECT * FROM wifi order by id");
+$stmt->execute(); $wifidb = $stmt->fetchAll();
+
+//Populate PS2 array
+$stmt = $edp_db->query("SELECT * FROM ps2 order by id");
+$stmt->execute(); $ps2db = $stmt->fetchAll();
+
+
+
+$localrev 		= exec("cd $workpath; svn info --username osxlatitude-edp-read-only --non-interactive | grep -i \"Last Changed Rev\"");
+$localrev 		= str_replace("Last Changed Rev: ", "", $localrev);
+//$remoterev      = exec("cd $workpath; svn info -r HEAD --username edp --password edp --non-interactive | grep -i \"Last Changed Rev\"");
+//$remoterev      = str_replace("Last Changed Rev: ", "", $remoterev);
+//$number_updates = ($remoterev - $localrev);	
+//$_SESSION['remoterev'] 		= $remoterev;
 
 // Include general functions
 require_once "$workpath/bin/functions.inc.php";
-
-//Checking if ONE of the pre-loaded vars is set, if not we will include loader.php to load them
-if ($rootpath == "") {
-	include "html/loader.php";
-}
-
-
 
 
 //Set timezone to UTC
@@ -58,4 +72,15 @@ if ($os == "") { echo "Unable to detect operating system, edptool has exited"; e
 
 $donateurl = "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=mail%40r2x2%2ecom&lc=US&item_name=OSXlatitude%20Donation&item_number=OSXLatitude%20Donation&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHostedGuest";
 
+
+//Configuration functions
+function getConfig($name) {
+    global $edp_db;
+    
+    $stmt = $edp_db->query("SELECT * FROM config where name = '$name'");
+    $stmt->execute();
+    $bigrow = $stmt->fetchAll();
+
+    return $bigrow[0]['value'];
+}
 ?>
