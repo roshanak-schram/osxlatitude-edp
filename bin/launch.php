ee5fe27a21494b19eee5232bb21c9c6ee2a64647
a.bouma <a.bouma@simulise.com>
2013-04-28 22:37:36 +0000

    Fixed bug when database does not exist on run.

--- bin/launch.php
+++ bin/launch.php
@@ -1,16 +1,21 @@
 <?php
+
 $workpath = "/Extra";
 
 //Check if db exists, and if not.. download it... 
-if (!is_file("$workpath/bin/edp.sqlite3")) { system("curl -o /Extra/bin/edp.sqlite3 http://www.osxlatitude.com/dbupdate.php"); }
-
+if (!file_exists("$workpath/bin/edp.sqlite3")) {
+    system("curl -o $workpath/bin/edp.sqlite3 http://www.osxlatitude.com/dbupdate.php");
+}
 
 include_once "config.inc.php";
 include_once "functions.inc.php";
+
 $os_string = "";
-$os        = getVersion();
+$os = getVersion();
 
-if ($os == "ml" || $os == "lion") { system("open /Extra/storage/apps/EDPweb.app"); }
-else { system("open http://127.0.0.1:11250/"); }
-
+if ($os == "ml" || $os == "lion") {
+    system("open /Extra/storage/apps/EDPweb.app");
+} else {
+    system("open http://127.0.0.1:11250/");
+}
 ?>
