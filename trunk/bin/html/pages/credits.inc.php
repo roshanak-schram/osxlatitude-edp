0162d983795ac14480e1ff61285c0985aa8e7467
info@alexbouma.me <info@alexbouma.me@c7ebc68b-1786-6683-9efc-5701743e9178>
2013-05-09 21:54:43 +0000

    Fixed some more typos

--- trunk/bin/html/pages/credits.inc.php
+++ trunk/bin/html/pages/credits.inc.php
@@ -13,10 +13,10 @@ echo "<div class='pageitem_bottom'>\n";
 <h2> .. to all of you that made EDP happened....</h2>
 <br>
 
-EDP is not just made by the team of OSXLatitude, its made by alot of people who have spend thousands and
+EDP is not just made by the team of OSXLatitude, it's made by a lot of people who have spend thousands and
 thousands of hours to make all this happened...
 
 <br><br>
 <b>Help us out....</b><br>
-Found something in here that you made ? Send a mail to <a href="mailto:lsb@osxlatitude.com">lsb@osxlatitude.com</a> and lets put on this page.
+Found something in here that you made? Send an email to <a href="mailto:lsb@osxlatitude.com">lsb@osxlatitude.com</a> and we will put you too on this page.
 
