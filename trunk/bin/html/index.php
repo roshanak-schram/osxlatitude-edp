ac6b28825f390e3e609cd9926b0395d9a1413bab
leon.bollerup@gmail.com <leon.bollerup@gmail.com@c7ebc68b-1786-6683-9efc-5701743e9178>
2013-06-12 21:30:43 +0000

    New index.php for EDP - Fixes for new EDP web app

--- trunk/bin/html/index.php
+++ trunk/bin/html/index.php
@@ -16,13 +16,6 @@ $edpmode = "web";
 	<script src="/edp.core.js.php" type="text/javascript"></script>
     <body onload="edp.bootloader();" onresize="aligndesign();" background="images/console_bg.png">
     	
-<div style="position: absolute; display: none; width: 100%; height: 100%; z-index: 10000; left: 50px; top: 0px; background-image: url('images/semi-transparent.png')" id="wait">
-	<table border="0" width="100%" cellpadding="0" style="border-collapse: collapse" height="100%">
-		<tr><td height="48%" width="48%"></td><td height="48%"></td><td height="48%" width="48%"></td></tr>
-		<tr><td width="48%">&nbsp;</td><td><p align="center"><img border="0" src="images/spinning-wheel.gif"><br><br><font color="#FFFFFF">Loading....</font><font></td><td width="48%">&nbsp;</td></tr>
-		<tr><td height="48%" width="48%">&nbsp;</td><td height="48%">&nbsp;</td><td height="48%" width="48%">&nbsp;</td></tr>
-	</table>	
-</div>
 
 <table border="0" width="100%" cellpadding="0" style="border-collapse: collapse" background="images/topbar-bg-normal2.png" height="69">
             <tr style="vertical-align: bottom;"  class="topbarmenu" align="center" style='cursor: hand'>
@@ -36,7 +29,7 @@ $edpmode = "web";
 
                 <td>&nbsp;</td>
                 <td width="80" onclick="loader('Credits');"><img src="icons/big/credits.png" width="40"></td>                
-                <td width="80"><a href='https://code.google.com/p/osxlatitude-edp/issues/list' target='_blank'><img src="icons/big/issues.png" width="40"></a></td>
+                <td width="80"><a href="http://code.google.com/p/osxlatitude-edp/issues/list" target="_blank"><img src="icons/big/issues.png" width="40"></a></td>
                 <td width="80"><a href='<?= "$donateurl"; ?>' target='_blank'><img src="icons/big/paypal.png" width="40"></a></td>
             </tr>
             <tr class="topbarmenu" align="center" style='cursor: hand'>
@@ -52,9 +45,9 @@ $edpmode = "web";
             </tr>
         </table>
 
-		<iframe id="edpmenu" class="edpmenu" marginwidth="0" marginheight="0" border="0" frameborder="0" height="80%" src="menu.inc.php?i=<?= "$i"; ?>"></iframe>
+		<iframe id="edpmenu" 		name="edpmenu" 			class="edpmenu" marginwidth="0" marginheight="0" border="0" frameborder="0" height="80%" src="menu.inc.php?i=<?= "$i"; ?>"></iframe>
 
-        <iframe id="console_iframe" class="console_iframe" marginwidth="0" marginheight="0" border="0" frameborder="0" src="show.php?i=pages/edp.inc.php"></iframe>
+        <iframe id="console_iframe" name="console_iframe" 	class="console_iframe" marginwidth="0" marginheight="0" border="0" frameborder="0" src="show.php?i=pages/edp.inc.php"></iframe>
 
 
 
