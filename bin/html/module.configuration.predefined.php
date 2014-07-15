492f177e0bf6c39aaa487c9b2f5c4b0c33f83106
emlydinesh <emlydinesh@c7ebc68b-1786-6683-9efc-5701743e9178>
2014-07-15 16:57:25 +0000

    Adding Modules and Themes

--- bin/html/module.configuration.predefined.php
+++ bin/html/module.configuration.predefined.php
@@ -208,7 +208,7 @@ if ($action == "") {
 	echoPageItemTOP("icons/big/config.png", "Select a model your wish to configure for:");
 
 	echo "<div class='pageitem_bottom'>\n";
-	echo "EDP's internal database contains 'best practice' schematics for 80+ systems - this makes it easy for to to choose the right configuration - however - you allways have the option to ajust the schematics before doing a build. <br><br>Doing a build means that EDP will copy a combination of kexts, dsdt, plists needed to boot your system.";
+	echo "EDP internal database contains 'best practice' configuration for the systems - this makes it easy to to choose the right configuration. However, you always have the option to ajust the configuration before doing a build. <br><br>Doing a build means that EDP will copy a combination of kexts, dsdt, plists, fixes and patches needed to boot your system.";
 
 	include "header.inc.php";
 	
