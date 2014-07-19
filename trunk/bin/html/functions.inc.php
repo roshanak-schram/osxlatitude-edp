01abd73ec4fc7dc84e337ec390c35f47a23399a9
leon.bollerup@gmail.com <leon.bollerup@gmail.com@c7ebc68b-1786-6683-9efc-5701743e9178>
2014-07-19 06:17:17 +0000

    Added new function that will check if system is running YOS, and if it does create a symbolic link from /SL/Kernels/kernel to /mach_kernel - this is a simple hack to ensure that we dont have to manually update all org.cham files..

--- trunk/bin/html/functions.inc.php
+++ trunk/bin/html/functions.inc.php
@@ -163,10 +163,22 @@ function echoPageItemTOP($icon, $text) {
     	system_call("chmod -R 755 \"$slepath/$kext\"");
     }
     
+    //
+    // Function to copy /SL/Kernels/kernel to /mach_kernel during build proess, funcion will check if OS is yos 10.10.x and copy a kernel only if needed
+    // To ensure correct operation, we will create a symbolic link the "real" kernel
+    public function yosLinkKernel() {
+		global $v;
+		if ($v == "yos") {
+			if (file_exists("/System/Library/Kernels/kernel") && (!file_exists("/mach_kernel"))) { system_call("ln -s /System/Library/Kernels/kernel /mach_kernel"); }
+		}    
+    }
+    
+    
 	//
 	// Get Value from Key in SMbios.plist
 	//
 	include_once __DIR__ . '/vendor/CFPropertyList/CFPropertyList.php';
+
 
 	function getValueFromSmbios($key, $default = null) {
 		global $workpath;
