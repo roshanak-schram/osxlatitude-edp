e8cda4686b4a10fb8a2fb6ef65c3703dddaf8b77
emlydinesh <emlydinesh@c7ebc68b-1786-6683-9efc-5701743e9178>
2014-07-20 20:12:38 +0000

    Fixed bugs in workpath for some functions

--- trunk/bin/update.sh
+++ trunk/bin/update.sh
@@ -10,7 +10,6 @@ rm -Rf edp.sqlite3
 echo "Downloading new database..."
 if curl -o edp.sqlite3 http://www.osxlatitude.com/dbupdate.php --connect-timeout 10 ; then
     echo "Database successfully downloaded <br>"
-    touch Updsuccess.txt
 else
     echo "Could not update database... using backup <br>"
     cp backup/edp.sqlite3 ./edp.sqlite3
