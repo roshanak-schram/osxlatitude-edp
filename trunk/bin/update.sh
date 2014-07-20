1e1178808f558b4809c09aec550296667f76e837
emlydinesh <emlydinesh@c7ebc68b-1786-6683-9efc-5701743e9178>
2014-07-20 12:49:08 +0000

    Fixed bugs in update and install methods

--- trunk/bin/update.sh
+++ trunk/bin/update.sh
@@ -1,3 +1,20 @@
+echo "<br>Updating database..."
+
+cd /Extra/EDP/bin
+echo "<br>Making a backup..."
+rm -Rf /Extra/EDP/bin/backup/edp.sqlite3
+cp edp.sqlite3 /Extra/EDP/bin/backup
+echo "Backup finished"
+rm -Rf edp.sqlite3
+
+echo "Downloading new database..."
+if curl -o edp.sqlite3 http://www.osxlatitude.com/dbupdate.php --connect-timeout 10 ; then
+    echo "Database successfully downloaded <br>"
+    touch Updsuccess.txt
+else
+    echo "Could not update database... using backup <br>"
+    cp backup/edp.sqlite3 ./edp.sqlite3
+fi
 
 echo "Cleaning up EDP svn... <br>"
 svn cleanup /Extra/EDP/bin
@@ -23,24 +40,6 @@ else
 	cd /Extra/EDP/logs/update
 	touch Updfail.txt
 	echo "Update failed (may be no internet or failed to connect to svn)"
-fi
-
-echo "<br>Updating database..."
-
-cd /Extra/EDP/bin
-echo "<br>Making a backup..."
-rm -Rf /Extra/EDP/bin/backup/edp.sqlite3
-cp edp.sqlite3 /Extra/EDP/bin/backup
-echo "Backup finished"
-rm -Rf edp.sqlite3
-
-echo "Downloading new database..."
-if curl -o edp.sqlite3 http://www.osxlatitude.com/dbupdate.php --connect-timeout 10 ; then
-    echo "Database successfully downloaded <br>"
-    touch Updsuccess.txt
-else
-    echo "Could not update database... using backup <br>"
-    cp backup/edp.sqlite3 ./edp.sqlite3
 fi
 
 chmod -R 755 /Extra/EDP/bin
