3c441abb6a080b723cd852d860bf23ebaffc61ae
emlydinesh <emlydinesh@c7ebc68b-1786-6683-9efc-5701743e9178>
2014-08-11 11:27:49 +0000

    Updated custom build process to be effective

--- trunk/bin/html/module.configuration.custom.php
+++ trunk/bin/html/module.configuration.custom.php
@@ -123,129 +123,161 @@
 // Build Process		
 function doCustomBuild() {
 
-	global $workPath;
-	global $eePath;
-	global $incPath;
+	$workPath = "/Extra/EDP";
+	$incPath = "/Extra/include";
+	$eePath = "/Extra/Extensions";
 	
 	$buildLogPath = "$workPath/logs/build";
 	
+	//
+	// Create log directory for build
+	//
+		
+	if(!is_dir("$workPath/logs"))
+    	system_call("mkdir $workPath/logs");
+    		
+	//
+	// Clear build log files
+	//
+	
+	if(!is_dir("$buildLogPath"))
+		system_call("mkdir $buildLogPath");
+	else {
+		system_call("rm -rf $buildLogPath/*");
+	}
+		
 	$cusoper = $_POST['copchoice'];
 	$edpoper = $_POST['eopchoice'];
 	
 	$dsdt = $_POST['dsdt']; $boot = $_POST['boot']; $smbios = $_POST['smbios'];
 	$ssdt = $_POST['ssdt']; $ssdt1 = $_POST['ssdt1']; $ssdt2 = $_POST['ssdt2']; $ssdt3 = $_POST['ssdt3']; $ssdt4 = $_POST['ssdt4'];
-
 	
 	//
     // check if myhack.kext exists in sle, if it dosent then copy it there...
     //
 	myHackCheck();
     
-    //
-	// Start by defining our log file and cleaning it..
-	//
-	$log = "$buildLogPath/build.log";
-	if (is_file("$log")) { 
-		system_call("rm -Rf $log"); 
-	}
-	
-	$myFixlog = "$buildLogPath/myFix.log";
-	if (is_file("$myFixlog")) { 
-		system_call("rm -Rf $myFixlog"); 
-	}
-    		
-	//writeToLog("$buildLogPath/build.log", "Choice: $cusoper, $edpoper<br>");
-
+	// writeToLog("$buildLogPath/build.log", "Choice: $cusoper, $edpoper<br>");
 	
 	if($cusoper == "cfullfix") {
-   		writeToLog("$buildLogPath/build.log", "<br><b>Calling myFix for Full fix to copy kexts and generate kernelcache</b><br><pre>");
-   		system_call("stty -tostop; sudo myfix -t / >>$buildLogPath/build.log 2>&1 &");
-   	 }
+
+		// Create myFix text file to start myFix process
+		writeToLog("$buildLogPath/quickFix.txt", "");
+		
+   		// Launch the script which provides the summary of the build process 
+		echo "<script> document.location.href = 'workerapp.php?action=showCustomBuildLog'; </script>";
+   	}
 	else if($cusoper == "cfixcache" || ($edpoper == "ebuild" && $cusoper == "cnone")) {
-   		writeToLog("$buildLogPath/build.log", "<br><b>Calling myFix for Quick fix to copy kexts and generate kernelcache</b><br><pre>");
-   		system_call("stty -tostop; sudo myfix -q -t / >>$buildLogPath/build.log 2>&1 &");
-   	 }
+   
+   		// Create myFix text file to start myFix process
+		writeToLog("$buildLogPath/fullFix.txt", "");
+		
+   		// Launch the script which provides the summary of the build process 
+		echo "<script> document.location.href = 'workerapp.php?action=showCustomBuildLog'; </script>";
+   	}
    		
 	else if($cusoper != "cnone")
 	{
-		//Step 1
-		writeToLog("$buildLogPath/build.log", "<br><b>Step 1) Checking if you have selected any sources from $incPath </b><br>");
+		// For log time
+		date_default_timezone_set("UTC");
+		$date = date("d-m-y H-i");
 
-		writeToLog("$buildLogPath/build.log", "Copying $incPath/Extensions to $eePath <br>");
-			
-			//Get all the kexts name in comma seperated way
-			$cclinfo = shell_exec("ls -m /Extra/include/Extensions/");
-    		$cckarray = explode(',', $cclinfo);
-				
-			$pckid = 100;
-			
-			foreach($cckarray as $ccfkname) {
-								
-				if ($ccfkname != "") {
-					
-				$ccfkname = preg_replace('/\s+/', '',$ccfkname); //remove white spaces
-					
-				if($_POST[$pckid] == "on") {
-					  system("cp -R /Extra/include/Extensions/$ccfkname $eePath");
-					  writeToLog("$buildLogPath/build.log", "Copying $ccfkname to $eePath<br>");
-					}
-					else {
-						if(is_dir("/Extra/Extensions/$ccfkname")) {
-							system("rm -rf /Extra/Extensions/$ccfkname");
-					  		writeToLog("$buildLogPath/build.log", "Removing $ccfkname from $eePath<br>");
-					  	}
-					}
-			 	}
-			 	$pckid++;
-			 }
+		system_call("echo '<br>*** Custom build logging started on: $date UTC Time ***' >> $buildLogPath/build.log");
 	
+		//
+		// Step 1
+		//
+		writeToLog("$buildLogPath/build.log", "<br><br><b>Step 1) Checking if you have selected any kexts from $incPath... </b><br>");
+			
+		// Get all the kexts name in comma seperated way
+		$cclinfo = shell_exec("ls -m /Extra/include/Extensions/");
+		$cckarray = explode(',', $cclinfo);
+			
+		$pckid = 100;
+		
+		foreach($cckarray as $ccfkname) {
+							
+			if ($ccfkname != "") {
+				
+			$ccfkname = preg_replace('/\s+/', '',$ccfkname); //remove white spaces
+				
+			if($_POST[$pckid] == "on") {
+				  system("cp -R /Extra/include/Extensions/$ccfkname $eePath");
+				  writeToLog("$buildLogPath/build.log", "Copying $ccfkname to $eePath<br>");
+				}
+				else {
+					if(is_dir("/Extra/Extensions/$ccfkname")) {
+						system("rm -rf /Extra/Extensions/$ccfkname");
+						writeToLog("$buildLogPath/build.log", "Removing $ccfkname from $eePath<br>");
+					}
+				}
+			}
+			$pckid++;
+		 }
+	
+		//
+		// Step 2
+		//
+		writeToLog("$buildLogPath/build.log", "<br><b>Step 2) Checking if you have essential files DSDT, SSDT and plists from $incPath... </b><br>");
+		
 		if ($dsdt == "on") {
-			writeToLog("$buildLogPath/build.log", "Copying $incPath/dsdt.aml to /Extra<br>");
-			system_call("cp -f /Extra/include/dsdt.aml /Extra");
+			writeToLog("$buildLogPath/build.log", "DSDT file found, Copying $incPath/DSDT.aml to /Extra<br>");
+			system_call("cp -f /Extra/include/DSDT.aml /Extra");
 		} 	
 	
     	if (is_file("/Extra/include/SSDT.aml") && $ssdt == "on") 					{ 
-    		writeToLog("$buildLogPath/build.log", "Copying SSDT file to /Extra<br>");
+    		writeToLog("$buildLogPath/build.log", "SSDT files(s) found, Copying SSDT.aml file to /Extra<br>");
     		system_call("cp -f /Extra/include/SSDT.aml /Extra"); 
     	}
     	if (is_file("/Extra/include/SSDT-1.aml") && $ssdt1 == "on") 				{ 
-    		writeToLog("$buildLogPath/build.log", "Copying SSDT-1 file to /Extra<br>");
+    		writeToLog("$buildLogPath/build.log", "Copying SSDT-1.aml file to /Extra<br>");
     		system_call("cp -f /Extra/include/SSDT-1.aml /Extra"); 
     	}
     	if (is_file("/Extra/include/SSDT-2.aml") && $ssdt2 == "on") 				{ 
-    		writeToLog("$buildLogPath/build.log", "Copying SSDT-2 file to /Extra<br>");
+    		writeToLog("$buildLogPath/build.log", "Copying SSDT-2.aml file to /Extra<br>");
     		system_call("cp -f /Extra/include/SSDT-2.aml /Extra"); 
     	}
     	if (is_file("/Extra/include/SSDT-3.aml") && $ssdt3 == "on") 				{ 
-    		writeToLog("$buildLogPath/build.log", "Copying SSDT-3 file to /Extra<br>");
+    		writeToLog("$buildLogPath/build.log", "Copying SSDT-3.aml file to /Extra<br>");
 			system_call("cp -f /Extra/include/SSDT-3.aml /Extra"); 
     	}    
     	if (is_file("/Extra/include/SSDT-4.aml") && $ssdt4 == "on") 				{ 
-    		writeToLog("$buildLogPath/build.log", "Copying SSDT-4 file to /Extra<br>");
+    		writeToLog("$buildLogPath/build.log", "Copying SSDT-4.aml file to /Extra<br>");
     		system_call("cp -f /Extra/include/SSDT-4.aml /Extra"); 
     	}
-
-		if ($smbios == "on")	{
-			writeToLog("$buildLogPath/build.log", "Copying $incPath/smbios.plist to /Extra<br>"); 
-			system_call("cp -f /Extra/include/smbios.plist /Extra");
-			}
 	
 		if ($boot == "on") { 
-			writeToLog("$buildLogPath/build.log", "Copying $incPath/org.chameleon.Boot.plist to /Extra<br>");
+			writeToLog("$buildLogPath/build.log", "Chameleon plist found, Copying $incPath/org.chameleon.Boot.plist to /Extra<br>");
 			system_call("cp -f /Extra/include/org.chameleon.Boot.plist /Extra");
-			}		
+		}	
+		if ($smbios == "on")	{
+			writeToLog("$buildLogPath/build.log", "SMBios file found, Copying $incPath/SMBios.plist to /Extra<br>"); 
+			system_call("cp -f /Extra/include/smbios.plist /Extra");
+		}	
 		
-		writeToLog("$buildLogPath/build.log", "<br><b>Step 2) Calling myFix to copy kexts and generate kernelcache</b><br><pre>");
-		system_call("stty -tostop; sudo myfix -q -t / >>$buildLogPath/build.log 2>&1 &");
-		writeToLog("$buildLogPath/build.log", "<a name='myfix'></a>");
+		//
+		// Step 3
+		//
+		writeToLog("$buildLogPath/build.log", "<br><b>Step 3) Calling myFix to copy kexts and generate kernelcache... </b><br>");
+		
+		// End build log and create a lastbuild log
+		system_call("echo '<br>*** Custom build logging ended on: $date UTC Time ***<br>' >> $buildLogPath/build.log");
+		system_call("cp $buildLogPath/build.log $workPath/logs/lastbuild.log ");
+		
+		// Append current build log to the builds log 
+		$fileContents = file_get_contents("$buildLogPath/build.log");
+		file_put_contents("$workPath/logs/build.log", $fileContents, FILE_APPEND | LOCK_EX);
+		
+		// Create myFix text file to start myFix process
+		writeToLog("$buildLogPath/quickFix.txt", "");
+		
+		// Launch the script which provides the summary of the build process 
+		echo "<script> document.location.href = 'workerapp.php?action=showCustomBuildLog'; </script>";
   
-    }
-    
-		writeToLog("$buildLogPath/build.log", "<a name='myfix'></a>");			
-		echo "<script> document.location.href = 'workerapp.php?action=showBuildLog#myfix'; </script>";
+      }
 	
 		exit;
-	}		
+ 	}		
 
 	//
 	// Custom configuration page
@@ -256,11 +288,11 @@ function doCustomBuild() {
 	echoPageItemTOP("icons/big/config.png", "Custom build configuration");
 
 	echo "<div class='pageitem_bottom'>\n";
-	echo "EDP custom configuration allows you to use your existing configuration in /Extra and allows you to choose your custom files included from /Extra/include.<br><br>";
-	echo "Copy your custom files like DSDT, SSDT, plists and boot(chameleon bootloader) to /Extra/include and <br> custom Kexts to /Extra/include/Extensions, which can be managed very effectively and easily using our EDP.<br>";
+	echo "EDP custom configuration allows you to use your existing configuration in /Extra and your custom files included from /Extra/include.<br><br>";
+	echo "Copy your custom files like DSDT, SSDT, plists and boot(chameleon bootloader) to /Extra/include and <br> custom Kexts to /Extra/include/Extensions, which can be managed here easily.<br>";
 	echo "</div>";
 			
-	//Load the tabs
+	// Load the tabs
 	echo "<script> $(function() { $( \"#tabs\" ).tabs(); }); </script>\n";
 		
 		// Show the tabs bar ?>
@@ -276,15 +308,20 @@ function doCustomBuild() {
 
 			echo "<div class='pageitem_bottom'><br>\n";
 
-			//Include tabs
+			// Include tabs
 			include "include/module.configuration.custom.files.inc.php";		
 			include "include/module.configuration.edp.files.inc.php";
 
 			echo "<input type='hidden' name='action' value='dobuild'>";
 			echo "</div><br>";
 			
-	echo "<ul class='pageitem'><li class='button'><input name='Submit input' type='submit' value='Do build!' /></li></ul><br><br>\n";
-	
-	exit;
-		
+	echo "<ul class='pageitem'><li class='button'><input name='Submit input' type='submit' value='Do build!' onclick='processmyFix();' /></li></ul><br><br>\n";
+			
 ?>
+
+	<script>
+	function processmyFix() {
+		
+		top.document.getElementById('edpmenu').src ='workerapp.php?action=showLoadingLog&type=custom';
+	}
+	</script>
