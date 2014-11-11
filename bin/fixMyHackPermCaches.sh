5c26f90657d305f9011480dc7ad97a6b73d507ee
emlydinesh <emlydinesh@c7ebc68b-1786-6683-9efc-5701743e9178>
2014-11-11 12:48:34 +0000

    Bug fixing

--- bin/fixMyHackPermCaches.sh
+++ bin/fixMyHackPermCaches.sh
@@ -6,6 +6,8 @@ cp -rf /Extra/EDP/myHack.kext /System/Library/Extensions
 
 echo "<br>Integrating kexts inide myHack kext..."
 cp -a /Extra/Extensions/. /System/Library/Extensions/myHack.kext/Contents/PlugIns/
+cp -a /Extra/Extensions/*.kext/Contents/PlugIns/ /System/Library/Extensions/myHack.kext/Contents/PlugIns/
+rm -rf /System/Library/Extensions/myHack.kext/Contents/PlugIns/*.kext/Contents/PlugIns
 cp -a /Extra/Extensions.EDPFix/. /System/Library/Extensions/myHack.kext/Contents/PlugIns/
 
 echo "<br>Fixing permissions..."
