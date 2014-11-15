f7a088771c2c68f24c39bc7f1e2aee9e274d8994
emlydinesh <emlydinesh@c7ebc68b-1786-6683-9efc-5701743e9178>
2014-11-15 11:48:44 +0000

    Fixed kernel cache rebuild issues

--- bin/fixPermCaches.sh
+++ bin/fixPermCaches.sh
@@ -3,10 +3,11 @@ echo "<b>Trying to fix System kexts permissions and rebuild caches...</b>"
 echo "<br>Fixing permissions..."
 chmod -R 755 /System/Library/Extensions
 chown -R root:wheel /System/Library/Extensions
-sudo touch /System/Library/Extensions
 
 echo "<br>Rebuilding caches..."
-rm -rf /System/Library/Caches/com.apple.kext.caches/Startup/kernelcache
-kextcache -prelinked-kernel /System/Library/Caches/com.apple.kext.caches/Startup/kernelcache -K /System/Library/Kernels/kernel /System/Library/Extensions
+rm -rf /System/Library/Caches/com.apple.kext.caches/Startup/*
+touch /System/Library/Extensions
+kextcache -system-prelinked-kernel
+kextcache -u /
 
 echo "<br><b>Fixing permissions and rebuilding caches are finished.</b>"
--- bin/fixPermCachesLegacy.sh
+++ bin/fixPermCachesLegacy.sh
@@ -3,10 +3,10 @@ echo "<b>Trying to fix system kexts permissions and rebuild caches...</b>"
 echo "<br>Fixing permissions..."
 chmod -R 755 /System/Library/Extensions
 chown -R root:wheel /System/Library/Extensions
-sudo touch /System/Library/Extensions
 
 echo "<br>Rebuilding caches..."
-rm -rf /System/Library/Caches/com.apple.kext.caches/Startup/kernelcache
-kextcache -prelinked-kernel /System/Library/Caches/com.apple.kext.caches/Startup/kernelcache -K /mach_kernel /System/Library/Extensions
+rm -rf /System/Library/Caches/com.apple.kext.caches/Startup/*
+touch /System/Library/Extensions
+kextcache -system-prelinked-kernel
 
 echo "<br><b>Fixing permissions and rebuilding caches are finished.</b>"
